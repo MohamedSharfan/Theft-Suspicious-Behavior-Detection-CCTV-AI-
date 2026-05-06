139a3aebb83d8055e2adda398c8e60e632c2d39c
Mohamed Sharfan <sharfansaleem72@gmail.com>
2026-05-06 20:05:41 +0530

    add unsupervised model

--- src/ml/predict.py
+++ src/ml/predict.py
@@ -3,10 +3,15 @@ import numpy as np
 
 class AnomalyDetector:
     def __init__(self):
-        self.model = joblib.load("models/anomaly_model.pkl")
+        self.model = joblib.load("./models/anomaly_model.pkl")
+        self.scaler = joblib.load("./models/scaler.pkl")
 
     def predict(self, features):
+        if len(features) != 4:
+            raise ValueError("Invalid feature lenght")
+        
         X = np.array([features])
-        pred = self.model.predict(X)
+        X = self.scaler.transform(X)
 
+        pred = self.model.predict(X)
         return 1 if pred[0] == -1 else 0
--- src/ml/train_anomaly.py
+++ src/ml/train_anomaly.py
@@ -1,17 +1,22 @@
 import pandas as pd
 from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
 import joblib
 
-df = pd.read_csv("data/raw/features.csv")
+df = pd.read_csv("./data/raw/features.csv")
 x = df[["speed", "direction", "acceleration", "time"]]
+
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
 
 model = IsolationForest(
     contamination=0.03,
     n_estimators=100
 )
 
-model.fit(x)
+model.fit(x_scaled)
 
-joblib.dump(model, "models/anomaly_model.pkl")
+joblib.dump(model, "./models/anomaly_model.pkl")
+joblib.dump(scaler, "./models/scaler.pkl")
 
 print("model trained")
